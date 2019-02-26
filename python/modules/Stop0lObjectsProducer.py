b038280ab00f9e48be151d6417694b1452e02a04
Zhenbin Wu <zhenbin.wu@gmail.com>
2019-02-25 22:17:46 -0600

    Adding 2018 DeepCSV WP

--- python/modules/Stop0lObjectsProducer.py
+++ python/modules/Stop0lObjectsProducer.py
@@ -8,9 +8,11 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 #2016 MC: https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation80XReReco#Data_MC_Scale_Factors_period_dep
 #2017 MC: https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation94X
+#2018 MC: https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation102X
 DeepCSVMediumWP ={
     "2016" : 0.6324,
     "2017" : 0.4941,
+    "2018" : 0.4184
 }
 
 CSVv2MediumWP = {
