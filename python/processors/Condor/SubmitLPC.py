af4de463e730adee96cb090cc74b27f3d2d4329a
Scarlet Norberg <scarletnorberg2014@gmail.com>
2020-07-23 17:18:31 -0500

    Does not seem to have picked up my change to postpost

--- python/processors/Condor/SubmitLPC.py
+++ python/processors/Condor/SubmitLPC.py
@@ -17,7 +17,7 @@ from collections import defaultdict
 from multiprocessing import Pool
 from itertools import izip_longest
 
-DelExe    = '../Stop0l_postproc.py'
+DelExe    = '../Stop0l_postpost.py'
 tempdir = '/uscmst1b_scratch/lpc1/3DayLifetime/%s/TestCondor/'  % getpass.getuser()
 ShortProjectName = 'PostProcess'
 VersionNumber = '_v7'
