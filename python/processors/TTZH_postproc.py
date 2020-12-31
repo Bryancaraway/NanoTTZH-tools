82914c2a36c77a73cee9b8a3adcbfcfac56868da
Bryan <bcaraway@cern.ch>
2020-12-30 19:49:26 -0600

    more re-run for Joe!

--- python/processors/TTZH_postproc.py
+++ python/processors/TTZH_postproc.py
@@ -124,7 +124,7 @@ def main(args):
         with open(args.inputfile) as f:
             files = [line.strip() for line in f]
 
-    p=PostProcessor(args.outputfile,files,cut=None, branchsel=None, outputbranchsel="keep_and_drop.txt", modules=mods,provenance=False,maxEntries=args.maxEvents, prefetch=True)
+    p=PostProcessor(args.outputfile,files,cut=None, branchsel=None, outputbranchsel="keep_and_drop.txt", modules=mods,provenance=False,maxEntries=args.maxEvents)#, prefetch=True)
     p.run()
 
 if __name__ == "__main__":
